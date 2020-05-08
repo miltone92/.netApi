e50dccc2ae48e669aed363e83626cc6c130f1a12
Milton Pineda <miltonpineda@MBP17C02V416UHTD7.local>
2020-05-08 12:53:38 -0600

    initial commit - working read/write user controller

---
+++ BbgApi/Models/MongoCRUD.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MongoDB.Driver;
+
+/*
+ * Purpose: Allows data base connectivity
+ * DB URL: "mongodb+srv://milton:milton@authordb-m9enp.mongodb.net/test?retryWrites=true&w=majority"
+ * mongodb+srv://milton:<password>@bbgcluster-m9enp.mongodb.net/test?retryWrites=true&w=majority
+ */
+
+namespace BbgApi.Models
+{
+    public class MongoCRUD
+    {
+
+        private IMongoDatabase db;
+
+        //constructor
+        public MongoCRUD(string database)
+        {
+            var client = new MongoClient("mongodb+srv://milton:milton@bbgcluster-m9enp.mongodb.net/test?retryWrites=true&w=majority");
+            db = client.GetDatabase(database);
+        }
+
+        //post
+        public void Post<T>(string table, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+            collection.InsertOne(record);
+
+        }
+    }
+}
+
---
+++ BbgApi/Models/UserModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace BbgApi.Models
+{
+    public class UserModel
+    {
+
+        //Attributess
+        [BsonId]
+        public Guid Id { get; set;}
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        //Constructor
+        public UserModel()
+        {
+        }
+
+        //Constructor
+        public UserModel(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+
+    }
+}
+
+
